6846283beb47077e69a4c83ab23118d285a123ef
Tatiana F. Pereira <tatianafp.unb@gmail.com>
2022-09-19 07:54:26 -0300

    add mais um exemplo

--- rodar.bat
+++ rodar.bat
@@ -1,2 +1,4 @@
 .\saida.exe --encode -i imagem.jpg -o bytes.txt 
 .\saida.exe --decode -i bytes.txt -o imagem_2.jpg 
+.\saida.exe --encode -i ABCD.txt -o ABCD_output.txt
+.\saida.exe --decode -i ABCD_output.txt -o ABCD_output2.txt
