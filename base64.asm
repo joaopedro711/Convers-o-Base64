8fb8501b641335da4f976ccade97e45d451d285c
João Pedro de Oliveira Silva <jo88791@gmail.com>
2022-09-20 18:26:05 -0300

    ASM Feito :+1:

--- base64.asm
+++ base64.asm
@@ -32,38 +32,43 @@ _main_base64_encode:
         pusha
 
         ; INICIO DA LOGICA
-        mov     ebx, 0
-        mov     ebx, [ebp+8]            ; ponteiro de dados recebe base da pilha + 8 bytes acima
+        ;                               A       B       C          D
+        ;Valor "ABCD" em binario eh 01000001 01000010 01000011 01000100
+        ;Vai receber primeiro COUNT,A,B,C. COUNT= 00000011 = 3
+        ;RECEBE depois COUNT,D,=,=. COUNT= 0000001 = 1
+        ;mov     edi, 00000011010000010100001001000011b 
+
+        mov     ebx, 0                  ; sera usado como uma variavel
+        mov     ebx, [ebp+8]            ; (parametro) ponteiro de dados recebe base da pilha + 8 bytes acima, valor referente a edi que sao os 4 bytes da mensagem a ser codificada
         shr     ebx, 24                 ; shift right, pega o valor de count
-        add     ebx, 1
+        add     ebx, 1                  ; pois tem uma posicao a mais devido COUNT
         mov     BYTE [ebp-4], bl        ; [ebp-4] representará, a partir de agora quantos caracteres ainda faltam ser gerados. bl = ebx(8bit)
-                                        ; Inicializa ele como count + 1
+                                        ; Inicializa ele como count + 1. bl = ebx, so que com 8 bits
 
         mov     ebx, 3                  ; ebx será utilizado para mutiplicar os delocamentos
         mov     eax, '===='             ; eax é a saída, inicia com os valores que suspostamente serao (pad).
-
 loop:
-        cmp     BYTE [ebp-4], 0         ; se nao faltar gerar mais nenhum byte
+        cmp     BYTE [ebp-4], 0         ; se nao faltar gerar mais nenhum byte, no inicio sao 4 devido que sao 24 bits/6 = 4
         je      fim                     ; vai encerrar a chamada do Assembly
 
-        mov     esi,[ebp+8]             ; Ponteiro para uma string de origem recebe base da pilha + 8 bytes. Pega o byte que sera convertido
+        mov     esi,[ebp+8]             ; (Pega o parametro) Ponteiro para uma string de origem recebe base da pilha + 8 bytes. Pega o byte que sera convertido
 
         mov     ecx, 6                  ; contador recebe o valor 6
-        imul    ecx, ebx                ; calculando deslocamento de count*6 bits
-        shr     esi, cl                 ; shift right de cl=8 bits 
+        imul    ecx, ebx                ; calculando deslocamento de count*6 bits. Inicio ebx = 3. 6*3=24, deslocamento de 24 bits
+        shr     esi, cl                 ; shift right de 24 bits, só que cl=8 bits
 
-        and     esi,0x3F                ; Extraindo 6 bits
+        and     esi,0x3F                ; Extraindo 6 bits, 0x3F= 00111111
         call    convert_caracter        ; chama a sub rotina pra converter 6 bits em caracter Base 64
 
         mov     ecx, 8                  ; contador recebe o valor 8
-        imul    ecx, ebx                ; calculando deslocamento de count*8 bits
-        shl     esi, cl                 ; shift left de cl=8bits
+        imul    ecx, ebx                ; calculando deslocamento de count*8 bits; No inicio valor ebx=3
+        shl     esi, cl                 ; tem o simbolo na base64. shift left de cl=8bits, retira os bits que ja foram convertidos
 
-        mov     edi, 0xFF               ; Ponteiro para uma string de destino recebe o valor 256
-        shl     edi, cl                 ; shift left de cl=8bits
+        mov     edi, 0x000000FF         ; Ponteiro para uma string de destino recebe o valor 256
+        shl     edi, cl                 ; shift left de cl=8bits do contador restante
         xor     edi, 0xFFFFFFFF         ; calculando máscara de bits
 
-        and     eax, edi                ; eax que eh o caracter "=" and a mascara de bits
+        and     eax, edi                ; eax que eh o caracter "=" and a mascara de bits, edi tem exatamente a posicao que o novo caracter tem que ficar em eax
         or      eax, esi                ; eax mais o caractere da Base 64
 
         sub     BYTE [ebp-4], 1         ; contador de bytes faltantes para conversão - 1
@@ -77,10 +82,10 @@ fim:
         ret                             
 
         
-;================================================================
-; convert_caracter:
-; Rotina interna para converter número de 6 bits em caractere base64
-;================================================================
+ ;================================================================
+ ; convert_caracter:
+ ; Rotina interna para converter número de 6 bits em caractere base64
+ ;================================================================
 convert_caracter:
         cmp     esi, 63                 ; n == 63?
         je      barra                   ; se sim, é uma /
@@ -99,7 +104,7 @@ mais:
         ret
 numeros:
         sub     esi, 52
-        add     esi, '0'                ; transforma no simbolo
+        add     esi, '0'                ; transforma no simbolo correspondente
         ret
 
 letras_maiusculas :
@@ -141,39 +146,40 @@ _main_base64_decode:
         pusha
 
         ; INICIO DA LOGICA
-        mov     ebx, 3                  ; recebe o valor 3 que representa 3 bytes
+        ;mov     edi, 'QUJDRA=='         ; Para debugar o codigo no programa SASM
+        mov     ebx, 3                  ; recebe o valor 3 que representa 3 bytes que deverao se tornar
         mov     eax, 0                  ; a saída recebe o valor 0, pois caso não tenha caracter é atribuido o valor 0
 loop2:
-        mov     esi,[ebp+8]             ; recebe os bytes para a decodificacao
+        mov     esi,[ebp+8]             ; (parametro) recebe os bytes para a decodificacao
 
         mov     ecx, 8                  ; contador recebe o valor 8
-        imul    ecx, ebx                ; calculando deslocamento de count*8 bits
-        shr     esi, cl                 ; shift right de 8 bits 
+        imul    ecx, ebx                ; calculando deslocamento de count*8 bits, no inicio 8*3=24
+        shr     esi, cl                 ; shift right de 8 bits menos significativos
 
         and     esi,0xFF                ; Extraindo 8 bits
-        cmp     esi, '='                ; se chegar em '=', é o final dos dados, ultimo caracter existente
+        cmp     esi, '='                ; se chegar em '=', é o final dos dados, ultimo caracter existente no arquivo
         je      fim_2                   ; 
 
         call    i_convert_caracter
 
-        mov     ecx, 6
+        mov     ecx, 6                  ; referente que sao 6 bits que se transformaram em base 64
         imul    ecx, ebx                ; calculando deslocamento de count*6 bits
-        shl     esi, cl
+        shl     esi, cl                 ; acrescenta zeros a direita dos 8 bits
 
-        mov     edi, 0x3F
-        shl     edi, cl
+        mov     edi, 0x3F               ;0x3F= 00111111
+        shl     edi, cl                 ; shift left de cl=8bits do contador restante
         xor     edi, 0xFFFFFFFF         ; calculando máscara de bits
 
-        and     eax, edi
-        or      eax, esi
-        add     eax, 0x01000000
+        and     eax, edi                ; vai pra posicao certa que deve colocar o caracter descodificado
+        or      eax, esi                ; esi tem o caracter decodificado em 6 bits
+        add     eax, 0x01000000         ; a saida recebe += 0000 0001 0000 0000 0000 0000 0000 0000, pra ficar com 8 bits
 
-        cmp     ebx, 0
+        cmp     ebx, 0                  ; se o contador chegar ao fim
         je      fim_2
-        sub     ebx, 1
+        sub     ebx, 1                  ;contador de quantos caracters restam -1
         jmp     loop2
 fim_2:
-        sub     eax, 0x01000000
+        sub     eax, 0x01000000         ; quando for o ultimo caracter ultrapassou os 8 bits devido ao carry, aí subtrai pra ficar certo
 
         ;ENCERRAMENTO DO PROGRAMA
         ;popa                           ;Nao sei a razao de nao funcionar
@@ -182,7 +188,7 @@ fim_2:
         ret                             
 ;================================================================
 ; i_convert_caracter:
-; Rotina inversa ao conver_esi. Será usada no processo de decode.
+; Rotina inversa ao convert_caracter. Será usada no processo de decode.
 ;================================================================
 i_convert_caracter:
         cmp     esi, '/'                ; é uma /?
